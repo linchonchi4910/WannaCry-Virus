c80c4e61704e5584536845c8879e2da6ce6d0379
linchonchi4910 <linchonchi@gmail.com>
2024-08-11 16:08:34 -0700

    Add files via upload

---
+++ WannaCry Virus/WannaCry.bat
@@ -0,0 +1,43 @@
+@echo off
+
+color a
+
+taskkill /f /im "explorer.exe"
+
+:1
+
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+
+set "text=Hello your device is infected with Wanna Cry, hit any key to proceed"
+
+echo speech.speak "%text%" >> "speech.vbs"
+
+start speech.vbs
+
+:2
+
+echo Hello your device is infected with Wanna Cry
+
+echo Hit any key to proceed
+
+pause
+
+:3
+
+del speech.Vbs
+
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+
+set "text=Lol just kidding it was a prank"
+
+echo speech.speak "%text%" >> "speech.vbs"
+
+start speech.vbs
+
+:4
+
+echo lol just kidding it was a prank
+
+start explorer.exe
+
+pause
